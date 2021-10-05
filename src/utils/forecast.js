bc9f90b02c16ed4c7c05ae10b3cfdd9d68eb895c
Casingal, Maria Pamela Nicole <maria.p.casingal@accenture.com>
2021-10-05 12:59:26 +0800

    Add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,9 +10,15 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location. Try another search.', undefined) //only works for the MOST ridiculous inputs like $$$$
             //someone suggested adding another else if > } else if (!response.body.location.name) {callback('No data for that location.', undefined)}
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.')
         }
     })
 };
 
 module.exports = forecast;
+
+//Goal: Add new data to forecast
+//1.Update the forecast string to include new data
+//2.Commit your changes
+//3.Push your changes to Github and deploy to Heroku
+//4.Test your work in the live application!
